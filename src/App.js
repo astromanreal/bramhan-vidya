b98f985ee57b116ae714c297349020225ca880f3
astromanreal <astroman@gmail.com>
2024-09-01 23:57:06 +0530

    updating files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import "./App.css";
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes, useLocation } from "react-router-dom";
+import ScrollToTop from "./components/utils/ScrollToTop";
+import Footer from "./components/home/Footer";
+import ExplorePage from "./components/home/ExplorePage";
+import Navbar from "./components/home/Navbar";
 import Home from "./components/home/Home";
 import Profiles from "./components/profiles/Profiles";
 import PlacesIndex from "./components/places/PlacesIndex";
 import UserRoutes from "./routes/UserRoutes";
+import communityRoutes from "./routes/communityRoutes";
 import ProfileRoutes from "./routes/ProfileRoutes";
 import PlaceRoutes from "./routes/PlaceRoutes";
 import TopicRoutes from "./routes/topicRoutes";
-import communityRoutes from "./routes/communityRoutes";
 import bookRoutes from "./routes/BookRoutes";
 import TechRoutes from "./routes/techRoutes";
 import eventRoutes from "./routes/eventRoutes";
 import blogRoutes from "./routes/BlogRoutes";
-import ExplorePage from "./components/home/ExplorePage";
-import Navbar from "./components/home/Navbar";
-import { useEffect } from "react";
-import ScrollToTop from "./components/utils/ScrollToTop";
 
 function App() {
   const location = useLocation();
@@ -44,6 +45,9 @@ function App() {
       />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="explore" element={<ExplorePage />} />
+        <Route path="profile" element={<Profiles />} />
+        <Route path="place" element={<PlacesIndex />} />
         {UserRoutes}
         {communityRoutes}
         {bookRoutes}
@@ -51,12 +55,10 @@ function App() {
         {eventRoutes}
         {TopicRoutes}
         {blogRoutes}
-        <Route path="profile" element={<Profiles />} />
         {ProfileRoutes}
-        <Route path="place" element={<PlacesIndex />} />
         {PlaceRoutes}
-        <Route path="explore" element={<ExplorePage />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/home/Footer.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div class="footer-container">
+          <div class="footer-intro">
+            <h2>Hinduism: A Journey of Discovery</h2>
+            <p>
+              Discover the amazing story of Hinduism and explore its rich and
+              diverse world, with its ancient roots, incredible history,
+              fascinating beliefs, traditions, and practices that have been
+              passed down for thousands of years, bringing joy, comfort, and
+              meaning to millions of people's lives around the world.
+            </p>
+          </div>
+          <div class="footer-section">
+            <h2>Explore</h2>
+            <ul>
+              <Link to="profile">Deities</Link>
+              <Link to="book">Scriptures</Link>
+              <Link to="place">Temples</Link>
+              <Link to="tech">Technology</Link>
+              <Link to="event">Events</Link>
+            </ul>
+          </div>
+          <div class="footer-section">
+            <h2>Features</h2>
+            <ul>
+              <Link to="blog">Blogs</Link>
+              <Link to="community">Community</Link>
+              <Link to="user">user guide</Link>
+              <Link to="user/signup">Join now!</Link>
+            </ul>
+          </div>
+          <div class="footer-section">
+            <h2>Resources</h2>
+            <ul>
+              <Link to="myprofile">My Account</Link>
+              <Link to="mycommunity">My community</Link>
+              <Link>Contact & Support</Link>
+              <Link>Privacy policy</Link>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-bottom">
+          <ul>
+            <Link to="">Home</Link>
+            <Link to="about">About</Link>
+            <Link to="explore">More...</Link>
+          </ul>
+          <p>&copy; 2024 Hinduism App</p>
+        </div>
+      </footer>
+    </>
+  );
+}
